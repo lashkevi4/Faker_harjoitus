f388f1782ab061832e7603ab67f5c9b9121e6e2a
lashkevi4 <lashkevi4@users.noreply.github.com>
2024-01-21 22:44:15 +0200

    add address for Faker task

--- Faker_harjoitus/Program.cs
+++ Faker_harjoitus/Program.cs
@@ -9,9 +9,22 @@ class Program
         {
             string firstName = Faker.Name.First();
             string lastName = Faker.Name.Last();
+            string address = Faker.Address.StreetAddress();
+            string personId = GeneratePersonId(lastName);
 
-            Console.WriteLine($"Имя: {firstName}, Фамилия: {lastName}");
+            Console.WriteLine($"Имя: {firstName}, Фамилия: {lastName}, Адрес: {address}, Идентификационный номер: {personId}");
         }
+
     }
+
+    static string GeneratePersonId(string lastName)
+    {
+        var firstLetter = lastName[0];
+        var numberPart1 = Faker.RandomNumber.Next(1000, 10000);
+        var numberPart2 = Faker.RandomNumber.Next(1000, 10000);
+
+        return $"{firstLetter} {numberPart1} {numberPart2}";
+    }
+
 }
 
