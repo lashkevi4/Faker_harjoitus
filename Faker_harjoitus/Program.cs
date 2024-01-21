71a43297504410260e48b97ca534bbcd1fc4d9d5
lashkevi4 <lashkevi4@users.noreply.github.com>
2024-01-21 20:55:44 +0200

    Faker task start

---
+++ Faker_harjoitus/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Faker;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            string firstName = Faker.Name.First();
+            string lastName = Faker.Name.Last();
+
+            Console.WriteLine($"Имя: {firstName}, Фамилия: {lastName}");
+        }
+    }
+}
+
